9896580e901ba5af22994f9aa73961c81c70803b
andrea <rosanovaandrea82@gmail.com>
2025-11-27 10:16:09 +0100

    Initial commit

---
+++ src/test/java/com/rosanova/iot/timer/TimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosanova.iot.timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
